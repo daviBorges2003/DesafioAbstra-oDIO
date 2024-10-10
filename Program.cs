e2800e2e03e1caaca8203dd9bf8e6c4459e1b2e8
davi.paula@hotmail.com <davi.paula@hotmail.com>
2024-10-10 16:52:39 -0300

    Desafio Resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
+Smartphone nokia = new Nokia("(24) 999999999","NoGy","464646465465465", 128);
+nokia.InstalarAplicativo("Tinder");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Nokia("(24) 988888888", "Pro Max" , "46464464987979", 256);
+iphone.InstalarAplicativo("Twitter");
+iphone.Ligar();
+iphone.ReceberLigacao();
 // TODO: Realizar os testes com as classes Nokia e Iphone
